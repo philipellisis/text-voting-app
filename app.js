ac9d2320b685ad6edf414db998a12e345d17c92f
Phil Ellis <philip.ellis.j@gmail.com>
2019-05-10 17:39:55 -0400

    added some basic authorization

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ var bodyParser  =     require('body-parser');
 
 
 // Twilio Credentials 
-var accountSid = ''; 
-var authToken = ''; 
+var accountSid = process.env.ACCOUNT_SID; 
+var authToken = process.env.AUTH_TOKEN; 
+var auth = process.env.AUTH;
 
 //require the Twilio module and create a REST client 
 var twillio = require('twilio')(accountSid, authToken); 
@@ -17,7 +18,7 @@ console.log('******************survey counter started******************');
 
 
 //start server and listen on predix port, or local port defined in settings
-var app    = express();
+var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 var server = app.listen(process.env.PORT || 5000, function () {
@@ -27,45 +28,57 @@ var server = app.listen(process.env.PORT || 5000, function () {
 
 app.post("/text", function (request, response) {
     console.log(request.body);
-    let surveyOptions = "";
-    for (x in surveySetup) {
-      surveyOptions = surveyOptions + '"' + surveySetup[x].id + '" for ' + surveySetup[x].name + '\n'
-    }
-    let messageText = "Unknown vote has been cast, please text the following: \n" + surveyOptions
-    if(surveyResults[request.body.From] === undefined) {
-      for (x in surveySetup) {
-        if(surveySetup[x].name.toUpperCase() === request.body.Body.toUpperCase() || surveySetup[x].id.toUpperCase() === request.body.Body.toUpperCase()){
-          messageText = "vote has been cast for " + surveySetup[x].name
-          surveyResults[request.body.From] = x
-        }
-      }
+    if(request.body.AccountSid !== accountSid) {
+      response.status(401).send("Unauthorized");
     } else {
+      let surveyOptions = "";
       for (x in surveySetup) {
-        if(surveySetup[x].name.toUpperCase() === request.body.Body.toUpperCase() || surveySetup[x].id.toUpperCase() === request.body.Body.toUpperCase()){
-          messageText = "vote has been changed from " + surveySetup[surveyResults[request.body.From]].name + " to " + surveySetup[x].name
-          surveyResults[request.body.From] = x
+        surveyOptions = surveyOptions + '"' + surveySetup[x].id + '" for ' + surveySetup[x].name + '\n'
+      }
+      let messageText = "Unknown vote has been cast, please text the following: \n" + surveyOptions
+      if(surveyResults[request.body.From] === undefined) {
+        for (x in surveySetup) {
+          if(surveySetup[x].name.toUpperCase() === request.body.Body.toUpperCase() || surveySetup[x].id.toUpperCase() === request.body.Body.toUpperCase()){
+            messageText = "vote has been cast for " + surveySetup[x].name
+            surveyResults[request.body.From] = x
+          }
+        }
+      } else {
+        for (x in surveySetup) {
+          if(surveySetup[x].name.toUpperCase() === request.body.Body.toUpperCase() || surveySetup[x].id.toUpperCase() === request.body.Body.toUpperCase()){
+            messageText = "vote has been changed from " + surveySetup[surveyResults[request.body.From]].name + " to " + surveySetup[x].name
+            surveyResults[request.body.From] = x
+          }
         }
       }
+  
+      twillio.messages.create({ 
+        to: request.body.From, 
+        from: "+12162085774", 
+        body: messageText, 
+      }).then(message => console.log(message));;
+  
+      response.send(messageText);
     }
 
-    twillio.messages.create({ 
-      to: request.body.From, 
-      from: "+12162085774", 
-      body: messageText, 
-    }).then(message => console.log(message));;
-
-    response.status = 200;
-    response.send(messageText);
   });
 
   app.get("/results", function(request, response) {
-    response.status = 200;
-    response.send(surveyResults);
+    if(request.headers.authorization !== auth) {
+      response.status(401).send("Unauthorized");
+    } else {
+      response.status = 200;
+      response.send(surveyResults);
+    }
+
   })
 
   app.post("/setup", function(request, response) {
-    surveyResults = {};
-    surveySetup = request.body;
-    response.status = 200;
-    response.send("");
+    if(request.headers.authorization !== auth) {
+      response.status(401).send("Unauthorized");
+    } else {
+      surveyResults = {};
+      surveySetup = request.body;
+      response.send("");
+    }
   });
